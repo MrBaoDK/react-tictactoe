cc40139b421740960108452b5abd4d6af17ea83d
Bao DK+FullStackWSL <MrBaoDK@hotmail.com>
2025-08-21 00:10:54 +0700

    fix: correct subbasename for github page deployment, fix deploy --cname argument

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import { BrowserRouter as Router } from "react-router-dom";
-
 import TicTacToeGame from "./component/Game";
 
 export default function App() {
-	return (<Router basename="/react-tictactoe">
-			<TicTacToeGame />
-		</Router>);
+	return (<TicTacToeGame />);
 }
